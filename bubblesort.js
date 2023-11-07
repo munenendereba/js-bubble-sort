76e17bbd027ce54404ea03108a93bb788f0cfe60
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2023-11-07 10:18:03 +0300

    testing sorted and unsorted array

--- bubblesort.js
+++ bubblesort.js
@@ -1,14 +1,20 @@
 const swap = require("./swap");
 
 const bubbleSort = (input) => {
+  let swapCount = 0;
   let swapping = true;
 
   while (swapping) {
     swapping = false;
-
-    for (let i = 0; i < input.length - 1; i++) {}
+    for (let i = 0; i < input.length - 1; i++) {
+      if (input[i] > input[i + 1]) {
+        swap(input, i, i + 1);
+        swapCount++;
+        swapping = true;
+      }
+    }
   }
-
+  console.log(`Swapped ${swapCount} times`);
   return input;
 };
 
